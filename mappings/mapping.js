2af4eb2d7db66dd421b7e0fca69e9caed9ddd286
rhinolary <39052241+Roguim@users.noreply.github.com>
2023-02-14 22:36:18 +1100

    New Optional Fields Object.
    To be used for validating types.

--- mappings/mapping.js
+++ mappings/mapping.js
@@ -4,11 +4,12 @@
 //  REQ compounds: Where we can find the compound mappings (e.g. AbilityMappings) - Splits at `/` character, treating that as another traversal down the tree.
 //      root: Where Player and Compounds both root off. If not specified, will be taken as root.
 const essentialFields = {"player": "test", "compounds": "text"};
+const optionalFields = {"root": "test"};
 
-//Modify in here please :)
+//Modify down below only please :) (unless you're changing how the base code works) -- This will just allow for proper mapping validation.
 //Feel free to use another JS file for this too, just as long as it gets imported into the mappings object properly!
 let asdMap = {"player": "9123",
             "compounds": "east/9123",
-            "root": "abs"};
+            "root": "123"};
 
 var mappings = {"asdf": asdMap};
